c89bad701fa0b5b39de6281f0b4bfd248ab859cb
Guillaume Yawo Kpotufe <mcguy2008@gmail.com>
2012-04-10 20:55:39 +0000

    Google auth works

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 /**@author : Yawo Guillaume Kpotufe
 */
+
 var express =  require('express');
 var app = express.createServer();
-
+var store  = new express.session.MemoryStore;
 //Authentication. (Passport)
 var passport = require('passport')
   , util = require('util')
@@ -16,16 +17,26 @@ var passport = require('passport')
   , GitHubStrategy = require('passport-github').Strategy;
 
 
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
 app.configure(function(){
+  app.set('dealit_host','127.0.0.1');
+  app.set('dealit_port',8855);
   app.use(express.methodOverride());
-  app.use(express.bodyParser());
-  app.use(app.router);
+  app.use(express.bodyParser()); 
   app.use(express.cookieParser());  
-  app.use(express.session({ secret: 'keyboard cat' }));
+  app.use(express.session({ secret: 'keyboard cat', store: store }));
   app.use(passport.initialize());
-  app.use(passport.session());
-  app.use(app.router);
+  app.use(passport.session());  
+   app.use(app.router);
   app.use(express.static(__dirname + '/../../public'));
+  
 });
 
 app.configure('development', function(){
@@ -39,8 +50,7 @@ app.configure('development', function(){
 app.configure('production', function(){
   var oneYear = 31557600000;
   app.use(express.static(__dirname + '/public', { maxAge: oneYear }));
-  app.use(express.errorHandler());
-  app.use(express.bodyParser());  
+  app.use(express.errorHandler());  
   app.set('redis_port',6415);
   app.set('redis_auth','0c9f2bc6e1565a3c1463052b6f3b7d53');  
   app.set('redis_host','127.0.0.1');
@@ -65,9 +75,59 @@ users.forEach(function (reply, i) {
     client.hset("dealit_users", reply.id, reply, redis.print);
 });
 
+//Auth Strategies
+passport.use(new GoogleStrategy({
+    returnURL: 'http://'+app.settings.dealit_host+':'+app.settings.dealit_port+'/auth/google/return',
+    realm: 'http://'+app.settings.dealit_host+':'+app.settings.dealit_port+'/'
+  },
+   function(identifier, profile, done) {
+    // asynchronous verification, for effect...
+    process.nextTick(function () {
+      
+      // To keep the example simple, the user's Google profile is returned to
+      // represent the logged-in user.  In a typical application, you would want
+      // to associate the Google account with a user record in your database,
+      // and return that user instead.
+      profile.identifier = identifier;
+      console.log("Google profile :",profile);
+      return done(null, profile);
+    });
+  }
+));
 
+// GET /auth/google
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  The first step in Google authentication will involve redirecting
+//   the user to google.com.  After authenticating, Google will redirect the
+//   user back to this application at /auth/google/return
+app.get('/auth/google', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  });
 
+app.get('/auth/google/callback', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  });
 
+// GET /auth/google/return
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  If authentication fails, the user will be redirected back to the
+//   login page.  Otherwise, the primary route function function will be called,
+//   which, in this example, will redirect the user to the home page.
+app.get('/auth/google/return', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  });
+
+app.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
 
  
 //Routes
@@ -76,9 +136,15 @@ app.get('/users/:ids', function(req, res){
     res.json({'user':req.params.ids});
 });
 
+app.get('/login', function(req, res){
+    res.send("Login page here."+req.user);
+});
+
 app.get('/', function(req, res){
-  res.send('hello world');	
+  name = (req.user)?req.user.displayName:'Anonymous';
+  res.send('Hello ' +name);	
+  req.session.currentUser=req.user;
 }); 
 
-app.listen(8855);
+app.listen(app.settings.dealit_port);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);

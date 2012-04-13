931b5591915fc4ef7820e5ceb4d17f462da238c8
Guillaume Yawo Kpotufe <mcguy2008@gmail.com>
2012-04-13 20:13:06 +0000

    all Auths are fine

--- server.js
+++ server.js
@@ -1,6 +1,37 @@
 /**@author : Yawo Guillaume Kpotufe
 */
+//Auth Strategies
+var developmentFACEBOOK_APP_ID = '203855983058663'; 
+var developmentFACEBOOK_APP_SECRET = '44b05c5bdf17e66f62bbdccd39e2c8fa';
+var productionFACEBOOK_APP_ID = '305865759484247'; //for cloudNode
+var productionFACEBOOK_APP_SECRET = '283fb39d66a3d7467c8ffe64a611c4f3';
 
+var developmentTWITTER_APP_ID = '2RX5bEubBLz5tT3XgHCg'; 
+var developmentTWITTERK_APP_SECRET = 'JWYm5K1PNLn3tF5or2MgqNWI3CGRlm65bpUdVwicw';
+var productionTWITTER_APP_ID = 'moIFY2hLYtnZpeRltsWkMw'; //for cloudNode
+var productionTWITTERK_APP_SECRET = 'iIvSzfLtzklmJKVg8ZJMCFND3ockTVfXdMQDzOtIM';
+
+
+var developmentWINDOWSLIVE_APP_ID = '000000004C09DC3E'; 
+var developmentWINDOWSLIVE_APP_SECRET = '3Mj-Jo943KGcQPaBtDxgayblzzNOW2iJ';
+var productionWINDOWSLIVE_APP_ID = '00000000400AA2B9'; //for cloudNode
+var productionWINDOWSLIVE_APP_SECRET = 'aPmu3vrxmhzwd5azgP4HsSiqHF8Baujx';
+
+var developmentLINKEDIN_APP_ID = 'qr471rqgojaa';
+var developmentLINKEDIN_APP_SECRET = 'xlNgQuKCjCCBQEoz';
+var productionLINKEDIN_APP_ID = 'jmxeb8tc23vo'; //for cloudNode
+var productionLINKEDIN_APP_SECRET = 'G2846x0c0iISx4Cn';
+
+var developmentTUMBLR_APP_ID = 'CkxN7oAtEUqMH8sbwolZFMCtWshDbTiTW8y4PdHnOc5Hhg6aUq'; 
+var developmentTUMBLR_APP_SECRET = 'kqydcLFkAjCtjS0mnPFx0Qj3st0InnL4msFE6n89GBDdoUNPLw';
+var productionTUMBLR_APP_ID = 'CkxN7oAtEUqMH8sbwolZFMCtWshDbTiTW8y4PdHnOc5Hhg6aUq'; //for cloudNode
+var productionTUMBLR_APP_SECRET = 'kqydcLFkAjCtjS0mnPFx0Qj3st0InnL4msFE6n89GBDdoUNPLw';
+
+
+var developmentGITHUB_APP_ID = '91684fc313c9a42cd0fd';
+var developmentGITHUB_APP_SECRET = '45d47bc38832518e6744f83c48a46c474b64d958';
+var productionGITHUB_APP_ID = 'fb71b7c825d222862e3c'; //for cloudNode
+var productionGITHUB_APP_SECRET = '9e4a82976d2213ce529218ee5c124bd81862d674';
 //SSL
 const crypto = require('crypto'),
       fs = require("fs"),
@@ -39,11 +70,11 @@ var certificate = "-----BEGIN CERTIFICATE-----\n"+
 
 //var credentials = crypto.createCredentials({key: privateKey, cert: certificate});
 
-var FACEBOOK_APP_ID = '203855983058663'; //for cloudNode
-var FACEBOOK_APP_SECRET = '44b05c5bdf17e66f62bbdccd39e2c8fa';
 
-var HEROKUFACEBOOK_APP_ID = '195253437258220';
-var HEROKUFACEBOOK_APP_SECRET = '4eeaf505678986059051f9c03b14ef6d';
+
+
+//var HEROKUFACEBOOK_APP_ID = '195253437258220';
+//var HEROKUFACEBOOK_APP_SECRET = '4eeaf505678986059051f9c03b14ef6d';
 
 
 //Authentication. (Passport)
@@ -56,6 +87,7 @@ var passport = require('passport')
   , WindowsLiveStrategy = require('passport-windowslive').Strategy
   , LinkedInStrategy = require('passport-linkedin').Strategy
   , TwitterStrategy = require('passport-twitter').Strategy
+  , TumblrStrategy = require('passport-tumblr').Strategy
   , GitHubStrategy = require('passport-github').Strategy;
 
 passport.serializeUser(function(user, done) {
@@ -71,9 +103,7 @@ var express =  require('express');
 var register = function(app,appPort,redisLocalPort,redisProdPort,secure){
 var protocol = (secure)?"https":"http";
 //Config.
-app.configure(function(){
-  app.set('dealit_host','localhost');
-  app.set('dealit_port',appPort);
+app.configure(function(){  
   app.set('delait_protocol',protocol);
   app.use(express.methodOverride());
   app.use(express.bodyParser()); 
@@ -89,6 +119,9 @@ app.configure(function(){
 app.configure('development', function(){
     app.use(express.static(__dirname + '/public'));
     app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+    app.set('dealit_host','localhost');
+    app.set('dealit_host_validUrl','www.dealitfree.com');
+    app.set('dealit_port',appPort);
     app.set('redis_port',redisLocalPort);
     app.set('redis_host','127.0.0.1');
   	app.set('redis_auth','0c9f2bc6e1565a3c1463052b6f3b7d53'); 
@@ -98,12 +131,17 @@ app.configure('production', function(){
   var oneYear = 31557600000;
   app.use(express.static(__dirname + '/public', { maxAge: oneYear }));
   app.use(express.errorHandler());  
+  app.set('dealit_host','dealit.cloudeno.de');
+  app.set('dealit_host_validUrl','dealit.cloudeno.de');
+  app.set('dealit_port',80);
   app.set('redis_port',redisProdPort);
   app.set('redis_auth','0c9f2bc6e1565a3c1463052b6f3b7d53');  
   app.set('redis_host','127.0.0.1');
 });
  
-//Routes
+/**
+   Routes
+*/
 
 // GET /auth/google
 //   Use passport.authenticate() as route middleware to authenticate the
@@ -152,7 +190,88 @@ app.get('/auth/facebook/callback',
     res.redirect('/');
   });
 
- 
+app.get('/auth/twitter', 
+  passport.authenticate('twitter', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  });
+
+app.get('/auth/twitter/callback', 
+  passport.authenticate('twitter', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  });
+
+ app.get('/auth/yahoo',
+  passport.authenticate('yahoo'),
+  function(req, res){
+    // The request will be redirected to Yahoo for authentication, so
+    // this function will not be called.
+  });
+
+app.get('/auth/yahoo/callback', 
+  passport.authenticate('yahoo', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  });
+
+app.get('/auth/windowslive',
+  passport.authenticate('windowslive' ,{ scope: ['wl.signin', 'wl.basic']} ),
+  function(req, res){
+    // The request will be redirected to Windows Live for authentication, so
+    // this function will not be called.
+  });
+
+app.get('/auth/windowslive/callback', 
+  passport.authenticate('windowslive', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  });
+
+app.get('/auth/linkedin',
+  passport.authenticate('linkedin'),
+  function(req, res){
+    // The request will be redirected to LinkedIn for authentication, so
+    // this function will not be called.
+  });
+
+app.get('/auth/linkedin/callback', 
+  passport.authenticate('linkedin', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  });
+
+app.get('/auth/github',
+  passport.authenticate('github'),
+  function(req, res){
+    // The request will be redirected to GitHub for authentication, so this
+    // function will not be called.
+  });
+
+app.get('/auth/github/callback', 
+  passport.authenticate('github', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  });
+
+app.get('/auth/tumblr',
+  passport.authenticate('tumblr'),
+  function(req, res){
+    // The request will be redirected to Tumblr for authentication, so this
+    // function will not be called.
+  });
+
+app.get('/auth/tumblr/callback', 
+  passport.authenticate('tumblr', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  });
+
 
 
 app.get('/users/:ids', function(req, res){
@@ -164,7 +283,7 @@ app.get('/login', function(req, res){
 });
 
 app.get('/',ensureAuthenticated, function(req, res){
-  name = (req.user)?req.user.displayName:'Anonymous';
+  name = (req.user)?(req.user.displayName||req.user):'Anonymous';
   res.send('Hello ' +name);	
   req.session.currentUser=req.user;
 }); 
@@ -179,8 +298,8 @@ console.log("Express server listening on port %d in %s mode", app.address().port
 
 var httpsApp  =  express.createServer({key: privateKey, cert: certificate});
 var httpApp   =  express.createServer();
-register(httpApp,8800,6379,6415);
-register(httpsApp,8855,6379,6415);
+register(httpApp,8855,6379,6415);
+//register(httpsApp,8800,6379,6415);
 
 //Connect to Redis Database 
 var redis = require("redis");
@@ -192,7 +311,7 @@ client.on("error", function (err) {
     process.exit(1);
 });
 
-//Users
+//Users for passport-local test...
 var users = [
     { id: 1, username: 'admin', password: 'nimda', email: 'mcguy2008@gmail.com' }
   , { id: 2, username: 'manager', password: '123456', email: 'mcguy2008@gmail.com' }
@@ -201,7 +320,8 @@ users.forEach(function (reply, i) {
     client.hset("dealit_users", reply.id, reply, redis.print);
 });
 
-//Auth Strategies
+
+
 passport.use(new GoogleStrategy({
     returnURL: httpApp.settings.delait_protocol+'://'+httpApp.settings.dealit_host+':'+httpApp.settings.dealit_port+'/auth/google/return',
     realm: httpApp.settings.delait_protocol+'://'+httpApp.settings.dealit_host+':'+httpApp.settings.dealit_port+'/'
@@ -220,14 +340,79 @@ passport.use(new GoogleStrategy({
   }
 ));
 
+
 passport.use(new FacebookStrategy({
-    clientID: FACEBOOK_APP_ID,
-    clientSecret: FACEBOOK_APP_SECRET,
+    clientID: eval(httpApp.settings.env+'FACEBOOK_APP_ID'),
+    clientSecret: eval(httpApp.settings.env+'FACEBOOK_APP_SECRET'),
     callbackURL: httpApp.settings.delait_protocol+'://'+httpApp.settings.dealit_host+':'+httpApp.settings.dealit_port+'/auth/facebook/callback'
   },
   function(accessToken, refreshToken, profile, done) {   
     console.log("Facebook profile :",profile); 
     return done(null, profile);    
+  }
+));
+
+passport.use(new TwitterStrategy({
+    consumerKey: eval(httpApp.settings.env+'TWITTER_APP_ID'),
+    consumerSecret: eval(httpApp.settings.env+'TWITTERK_APP_SECRET'),
+    callbackURL: httpApp.settings.delait_protocol+'://'+httpApp.settings.dealit_host+':'+httpApp.settings.dealit_port+'/auth/twitter/callback'
+  },
+  function(token, tokenSecret, profile, done) {
+    console.log("Twitter profile :",profile); 
+    return done(null, profile);       
+  }
+));
+passport.use(new YahooStrategy({
+    returnURL: httpApp.settings.delait_protocol+'://'+httpApp.settings.dealit_host+':'+httpApp.settings.dealit_port+'/auth/yahoo/callback',
+    realm: httpApp.settings.delait_protocol+'://'+httpApp.settings.dealit_host+':'+httpApp.settings.dealit_port
+  },
+  function(identifier, profileId, done) {
+    console.log("Yahoo profile :",profileId); 
+    return done(null, profileId);      
+  }
+));
+
+//WindowsLive want valid url and localhost is not working, so I created a mapping dealitfree.com => 127.0.0.1 in the etc/host file.
+passport.use(new WindowsLiveStrategy({
+    clientID: eval(httpApp.settings.env+'WINDOWSLIVE_APP_ID'),
+    clientSecret: eval(httpApp.settings.env+'WINDOWSLIVE_APP_SECRET'),
+    callbackURL: httpApp.settings.delait_protocol+'://'+httpApp.settings.dealit_host_validUrl+':'+httpApp.settings.dealit_port+'/auth/windowslive/callback'
+  },
+  function(accessToken, refreshToken, profile, done) {
+    console.log("WindowsLive profile :",profile); 
+    return done(null, profile);         
+  }
+));
+passport.use(new LinkedInStrategy({
+    consumerKey: eval(httpApp.settings.env+'LINKEDIN_APP_ID'),
+    consumerSecret: eval(httpApp.settings.env+'LINKEDIN_APP_SECRET'),
+    callbackURL: httpApp.settings.delait_protocol+'://'+httpApp.settings.dealit_host+':'+httpApp.settings.dealit_port+'/auth/linkedin/callback'
+  },
+  function(token, tokenSecret, profile, done) {  
+  console.log("LinkedIn profile :",profile) ;  
+      return done(null, profile);    
+  }
+));
+
+passport.use(new GitHubStrategy({
+    clientID: eval(httpApp.settings.env+'GITHUB_APP_ID'),
+    clientSecret: eval(httpApp.settings.env+'GITHUB_APP_SECRET'),
+    callbackURL: httpApp.settings.delait_protocol+'://'+httpApp.settings.dealit_host+':'+httpApp.settings.dealit_port+'/auth/github/callback'
+  },
+  function(accessToken, refreshToken, profile, done) {   
+      console.log("GitHub profile :",profile) ;  
+      return done(null, profile);    
+  }
+));
+
+passport.use(new TumblrStrategy({
+    consumerKey: eval(httpApp.settings.env+'TUMBLR_APP_ID'),
+    consumerSecret: eval(httpApp.settings.env+'TUMBLR_APP_SECRET'),
+    callbackURL: httpApp.settings.delait_protocol+'://'+httpApp.settings.dealit_host+':'+httpApp.settings.dealit_port+'/auth/tumblr/callback'
+  },
+  function(token, tokenSecret, profile, done) {   
+      console.log("Tumblr profile :",profile) ;
+      return done(null, profile);    
   }
 ));
 
